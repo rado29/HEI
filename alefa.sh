09a38a9e39e140db88d5cb1217dc2e445e3438c5
root <rado.randriamanantsoa@gmail.com>
2025-06-10 07:52:09 +0000

    files

--- alefa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-chmod +x script1.sh script2.sh script3.sh
-echo "[*] Lancement des scripts..."
-./script1.sh &
-./script2.sh &
-./script3.sh &
-echo "[*] Tous les scripts ont été lancés. Bonne chance pour retrouver les 3 clés !"
-
